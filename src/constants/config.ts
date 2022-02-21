5cc0c59f20c127d74b99c09f7aaa4c8446a9189d
UmaLanguageProject <100102657+UmaLanguageProject@users.noreply.github.com>
2022-02-20 23:20:13 -0600

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: "Uma' - Anũpa' Estwasúhah", // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  author: 'Kate Pewenofkit Briner for Houma Language Project', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'https://www.houmalanguageproject.com/home', // Put a link to your website or social media here
+  wordListSource: 'Uma-English Dictionary', // Describe the source material for your words here
+  wordListSourceLink: 'https://play.google.com/store/apps/details?id=org.houmalanguageproject.umaenglishdictionary.uma&fbclid=IwAR2XFxnqDw7n6LLhIN3Yl1m6Q0nAHIsMJEnOP5zyfiYz0EjnBRbDQap6fks', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
